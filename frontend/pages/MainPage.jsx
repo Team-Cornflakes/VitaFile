a362173f7f22cd6d3c56f9eec87d075995b51c78
utkarsh <utkarsh.dhagat2022@vitstudent.ac.in>
2024-01-27 19:24:46 +0530

    3rd page header import fixed

--- frontend/pages/MainPage.jsx
+++ frontend/pages/MainPage.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import Header from '../components/Header';
-import LeftComponent from '../components/LeftComponent';
-import RightComponent from '../components/RightComponent';
-import './MainPage.css'; // Assuming you have a CSS file for MainPage
+import LeftComponent from '../components/LeftComponent'; // Adjust the path as necessary
+import RightComponent from '../components/RightComponent'; // Adjust the path as necessary
 
 const MainPage = () => {
-  return (
-    <div>
-      <Header />
-      <div className="main-content">
-        <Header/>
-        <LeftComponent />
-        <RightComponent />
-      </div>
-    </div>
-  );
+    return (
+        <div style={{ display: 'flex', height: '100vh' }}>
+            <LeftComponent />
+            <RightComponent />
+        </div>
+    );
 };
 
 export default MainPage;
