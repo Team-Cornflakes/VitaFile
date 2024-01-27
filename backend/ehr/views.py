c368730c0858f6813424c404b8b1d7b9ba1feed4
Kedar <kedarshinde8011@gmail.com>
2024-01-27 13:17:42 +0530

    fixes

--- backend/ehr/views.py
+++ backend/ehr/views.py
@@ -10,7 +10,7 @@ User = get_user_model()
 class UserEHRView(APIView):
     def get(self, request):
         user = request.user
-        user_ehr = EHR.objects.filter(user=user)
+        user_ehr = EHR.objects.filter(userid=user)
 
         # Convert the QuerySet to a list of dictionaries
         user_ehr_list = list(user_ehr.values())
