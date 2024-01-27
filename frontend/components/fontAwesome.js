a38d6ed22c85b157bcf0530cf00982e5d2730114
utkarsh <utkarsh.dhagat2022@vitstudent.ac.in>
2024-01-27 11:25:47 +0530

    deleted font thing

--- frontend/components/fontAwesome.js
+++
@@ -1,5 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCloudUpload } from '@fortawesome/free-solid-svg-icons';
-
-// Add icons to the library
-library.add(faCloudUpload);
