117ed05c52b2cba5d0a47be672cdccba18d99af0
Kedar <kedarshinde8011@gmail.com>
2024-01-27 12:39:39 +0530

    minor fixes search is wip

---
+++ backend/ehr/search_indexes.py
@@ -0,0 +1,6 @@
+from haystack import indexes
+from ehr.models import EHR
+
+class EHRIndex(indexes.SearchIndex, indexes.Indexable):
+    name = indexes.CharField(document=True, use_template=True)
+    
