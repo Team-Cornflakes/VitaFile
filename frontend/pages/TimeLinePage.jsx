b2c0df9fa0725797d01ba3000b134adf8636d1a9
codexx07 <sparsh.riju@gmail.com>
2024-01-28 11:14:40 +0530

    dk what to do

--- frontend/pages/TimeLinePage.jsx
+++ frontend/pages/TimeLinePage.jsx
@@ -3,13 +3,16 @@ import UploadCard from "../components/UploadCard.jsx";
 import Header from '../components/Header'; // Adjust the path as per your file structure
 import Sidebar from '../components/Sidebar'; // Adjust the path as per your file structure
 import '../src/App.css';
+import './TimeLinePage.css';
 
 export default function MyComponent() {
     return (
-        <div>
-            <Header />
-            <Sidebar />
-            <UploadCard/>
+        <div className="container">
+             <Header/>
+            <div className="Parent1">
+                <UploadCard />
+                <Sidebar /> 
+            </div>
         </div>
     );
 }
